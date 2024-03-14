039d9f639cf21988c11dca02d9d7f5c043959003
HAMADENAB <nabil.hamade3@ontario.ca>
2024-03-14 14:15:27 -0400

    clean up

--- src/main/resources/118881.sql
+++ src/main/resources/118881.sql
@@ -1,1 +1,1 @@
-update casereview set approvedby='IAAP117059', approveddatetime=to_date('10/04/2023 00:00:00','DD/MM/YYYY HH24:MI:SS'), approvalstatus='ARS1' where caseid = (select caseid from caseheader where casereference = 7301521) and casereviewreference = 6778168 and reasoncode='CR7';
+update CASEREVIEWINFO set EAPMILESTONEACHIEVEDIND = 1 where CASEREVIEWID = (select casereviewid from casereview where caseid = (select caseid from caseheader where casereference = 7288501)and casereviewreference = 5951595 and reasoncode='CR14')
